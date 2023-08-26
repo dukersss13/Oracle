4d22bdfbcb3e5e6bf905bacf10e3e64cad75438b
dukersss13 <60716171+dukersss13@users.noreply.github.com>
2023-08-25 23:55:57 -0700

    WIP

--- experiments.py
+++ experiments.py
@@ -3,14 +3,12 @@
 # 666 scores to evaluate RMSE's (2 scores for each playing team)
 
 import pandas as pd
-from models.oracle import Oracle
 from config import oracle_config, nn_config, xgboost_config
 
 
-all_logs = pd.read_csv("data/all_logs.csv")
+all_logs = pd.read_csv("data/all_logs.csv", index_col=0)
 latest_season = "2022-23"
 
 latest_season_games = all_logs[all_logs["SEASON_YEAR"]==latest_season]
 unique_games = latest_season_games["GAME_ID"].unique()
 experiment_game_ids = []
-
